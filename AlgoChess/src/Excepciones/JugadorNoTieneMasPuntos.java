855a857283999a9c6eb6a68cfb7ca6a8bad4845c
MarianaAyelen <marianayelen_06@hotmail.com>
2019-11-04 22:32:05 -0300

    excepciones

---
+++ AlgoChess/src/Excepciones/JugadorNoTieneMasPuntos.java
@@ -0,0 +1,6 @@
+package Excepciones;
+
+public class JugadorNoTieneMasPuntos extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ AlgoChess/src/Unidades/unidadSeQuedaSinVida.java
@@ -0,0 +1,5 @@
+package Unidades;
+
+public class unidadSeQuedaSinVida extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+}
