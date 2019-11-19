49518bf221d3a9c5a48c699204e285c253a61185
MarianaAyelen <marianayelen_06@hotmail.com>
2019-11-19 11:07:17 -0300

    Cambios en curandero

--- AlgoChess/src/Unidades/Curandero.java
+++ AlgoChess/src/Unidades/Curandero.java
@@ -1,5 +1,8 @@
 package Unidades;
 
+import Excepciones.CuranderoSoloCuraADistanciaMedia;
+import Excepciones.JugadorNoPuedeCurarAunidadEnemiga;
+
 public class Curandero extends UnidadCuradora {
 
 		public Curandero() {
@@ -7,5 +10,20 @@ public class Curandero extends UnidadCuradora {
 			vida = 75;
 			curacion = 15;	
 			movilidad = new unidadMovil();
+		}
+		
+		public void realizarComportamiento(Unidad unaUnidad) {
+			chequearCuracionAUnidadAliada(unaUnidad);
+			int distancia = this.calcularDistancia(unaUnidad);
+			if(distancia < 3 || distancia > 5) {
+				throw new CuranderoSoloCuraADistanciaMedia();
+			}
+			unaUnidad.sumarVida(curacion);
+		}
+		
+		protected void chequearCuracionAUnidadAliada(Unidad unaUnidad) {
+			if (this.obtenerPropietario() != unaUnidad.obtenerPropietario()) {
+				throw new JugadorNoPuedeCurarAunidadEnemiga();
+			}
 		}
 }
--- AlgoChess/src/Unidades/UnidadCuradora.java
+++ AlgoChess/src/Unidades/UnidadCuradora.java
@@ -8,17 +8,9 @@ public class UnidadCuradora extends Unidad {
 	protected int curacion; 
 	
 	public void realizarComportamiento(Unidad unaUnidad) {
-		chequearCuracionAUnidadAliada(unaUnidad);
-		int distancia = this.calcularDistancia(unaUnidad);
-		if(distancia < 3 || distancia > 5) {
-			throw new CuranderoSoloCuraADistanciaMedia();
-		}
-		unaUnidad.sumarVida(curacion);
-	}
+			}
 	
 	protected void chequearCuracionAUnidadAliada(Unidad unaUnidad) {
-		if (this.obtenerPropietario() != unaUnidad.obtenerPropietario()) {
-			throw new JugadorNoPuedeCurarAunidadEnemiga();
-		}
+		
 	}
 }
